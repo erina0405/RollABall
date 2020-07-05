533c39a2ba6b05598df970ee251e877c1ce541a8
erina0405 <erina.n0125@gmail.com>
2020-07-05 15:56:03 +0900

    Title

--- RollABall2020/Assets/Scripts/GotoGameScene.cs
+++ RollABall2020/Assets/Scripts/GotoGameScene.cs
@@ -1,18 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GotoGameScese : MonoBehaviour
+public class GotoGameScene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public void StartGameScene()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("GameScene");
     }
 }
