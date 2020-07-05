9ef262bd777254d55ef6a067de8172b590f9399f
erina0405 <erina.n0125@gmail.com>
2020-07-05 16:13:38 +0900

    Scene

---
+++ RollABall2020/Assets/Scripts/GoToTitleScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToTitleScene : MonoBehaviour
+{
+    public void ReturnTitleScene()
+    {
+        SceneManager.LoadScene("Title");
+    }
+
+   
+}
